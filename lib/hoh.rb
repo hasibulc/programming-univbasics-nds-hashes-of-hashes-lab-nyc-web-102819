efe7432e192e97ea62bf617a979462b9af799a90
Hasibul Chowdhury <hasibulc91@gmail.com>
2019-10-25 03:46:44 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -57,7 +57,7 @@ def naming_system
                     sub_category: {
                       label: 'Genus',
                       sub_category: {
-                        label: 'Species',
+                        label: 'Species'
                       }
                     }
                   }
